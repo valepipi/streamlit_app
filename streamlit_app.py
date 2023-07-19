e299cd9f132d970eaabfd7f7e20da891e03650a7
Jasmine <savoki163@163.com>
2023-07-19 13:18:58 +0800

    rename

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ with st.form("my_form"):
     text = st.text_area("Enter text:", "What are the three key pieces of advice for learning how to code?")
     submmited = st.form_submit_button("Submit")
     if not openai_api_key.startswith("sk-"):
-        st.warning("Please enter your OpenAI key!", icon="⚠'")
+        st.warning("Please enter your OpenAI key!")
     if submmited and openai_api_key.startswith("sk-"):
         generate_response(text)
 
